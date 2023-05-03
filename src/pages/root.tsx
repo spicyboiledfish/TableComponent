1cd1b8d6d0f9f7e20e8c46188b2362b9beac0564
luochangan <luochangan@bilibili.com>
2023-05-03 22:53:01 +0800

    Updates

--- src/pages/root.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from '@/pages/home';
-import '@/style/index.less';
-
-ReactDOM.render(<Home />, document.querySelector('#app') as HTMLDivElement);
